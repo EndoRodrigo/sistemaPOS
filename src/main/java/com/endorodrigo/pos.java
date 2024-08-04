5a2ab35658cc52f2cbaea24b2fe6277b61794377
EndoRodrigo <endorodrigo19@gmail.com>
2024-08-03 20:12:00 -0500

    feat: Creacion de la interfaz de el sistema pos

---
+++ src/main/java/com/endorodrigo/pos.java
@@ -0,0 +1,40 @@
+package com.endorodrigo;
+
+import javax.swing.*;
+
+public class pos {
+    private JPanel panelMain;
+    private JLabel title;
+    private JTextField txtInput;
+    private JList list1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button14;
+    private JButton button15;
+    private JButton button12;
+    private JButton button13;
+    private JButton button16;
+    private JButton button17;
+    private JButton button18;
+    private JButton button19;
+    private JButton button20;
+    private JLabel txtVersion;
+    private JLabel title2;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("pos");
+        frame.setContentPane(new pos().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
